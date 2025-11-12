babf69c30c3304d41c83d029925dc355ab131459
Rohitshah507 <shahaaditya1111@gmail.com>
2025-11-12 10:36:29 +0545

    Commit

---
+++ Operator/Ifelse.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class IfElse
+    {
+        public static void Main(String[] args)
+        {
+            Console.Write("Enter your age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            if (age < 13)
+            {
+                Console.WriteLine("Child");
+            }
+            else if (age >= 13 && age <= 19)
+            {
+                Console.WriteLine("Teenager");
+            }
+            else if (age >= 20)
+            {
+                Console.WriteLine("Adult");
+            }
+            else
+            {
+                Console.WriteLine("Invalid age entered!");
+            }
+        }
+    }
+}
